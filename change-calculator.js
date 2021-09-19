57c441ca997a8dff11056a80ede4abedca3a9ecb
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-09-19 09:58:59 +0700

    repositioned else statement

--- change-calculator.js
+++ change-calculator.js
@@ -8,82 +8,82 @@ function calculateChange(amount, money) {
     console.log(
       `\nKembalian yang harus diberikan kasir Rp. ${change} \ndibulatkan menjadi Rp. ${changeToBePaid}`
     );
+
+    let calculateSeratusRibu = Math.floor(changeToBePaid / 100000);
+    if (calculateSeratusRibu === 0) {
+    } else {
+      console.log(`${calculateSeratusRibu} lembar 100000`);
+    }
+    let changeRemaining = changeToBePaid % 100000;
+
+    let calculateLimapuluhRibu = Math.floor(changeRemaining / 50000);
+    if (calculateLimapuluhRibu === 0) {
+    } else {
+      console.log(`${calculateLimapuluhRibu} lembar 50000`);
+    }
+    changeRemaining = changeRemaining % 50000;
+
+    let calculateDuaPuluhRibu = Math.floor(changeRemaining / 20000);
+    if (calculateDuaPuluhRibu === 0) {
+    } else {
+      console.log(`${calculateDuaPuluhRibu} lembar 20000`);
+    }
+    changeRemaining = changeRemaining % 20000;
+
+    let calculateSepuluhRibu = Math.floor(changeRemaining / 10000);
+    if (calculateSepuluhRibu === 0) {
+    } else {
+      console.log(`${calculateSepuluhRibu} lembar 10000`);
+    }
+    changeRemaining = changeRemaining % 10000;
+
+    let calculateLimaRibu = Math.floor(changeRemaining / 5000);
+    if (calculateLimaRibu === 0) {
+    } else {
+      console.log(`${calculateLimaRibu} lembar 5000`);
+    }
+    changeRemaining = changeRemaining % 5000;
+
+    let calculateDuaRibu = Math.floor(changeRemaining / 2000);
+    if (calculateDuaRibu === 0) {
+    } else {
+      console.log(`${calculateDuaRibu} lembar 2000`);
+    }
+    changeRemaining = changeRemaining % 2000;
+
+    let calculateSeribu = Math.floor(changeRemaining / 1000);
+    if (calculateSeribu === 0) {
+    } else {
+      console.log(`${calculateSeribu} koin 1000`);
+    }
+    changeRemaining = changeRemaining % 1000;
+
+    let calculateLimaRatus = Math.floor(changeRemaining / 500);
+    if (calculateLimaRatus === 0) {
+    } else {
+      console.log(`${calculateLimaRatus} koin 500`);
+    }
+    changeRemaining = changeRemaining % 500;
+
+    let calculateDuaRatus = Math.floor(changeRemaining / 200);
+    if (calculateDuaRatus === 0) {
+    } else {
+      console.log(`${calculateDuaRatus} koin 200`);
+    }
+    changeRemaining = changeRemaining % 200;
+
+    let calculateSeratus = Math.floor(changeRemaining / 100);
+    if (calculateSeratus === 0) {
+    } else {
+      console.log(`${calculateSeratus} koin 100`);
+    }
+    changeRemaining = changeRemaining % 100;
   } else {
     console.log("\nMaaf, kurang bayar!");
   }
-
-  let calculateSeratusRibu = Math.floor(changeToBePaid / 100000);
-  if (calculateSeratusRibu === 0) {
-  } else {
-    console.log(`${calculateSeratusRibu} lembar 100000`);
-  }
-  let changeRemaining = changeToBePaid % 100000;
-
-  let calculateLimapuluhRibu = Math.floor(changeRemaining / 50000);
-  if (calculateLimapuluhRibu === 0) {
-  } else {
-    console.log(`${calculateLimapuluhRibu} lembar 50000`);
-  }
-  changeRemaining = changeRemaining % 50000;
-
-  let calculateDuaPuluhRibu = Math.floor(changeRemaining / 20000);
-  if (calculateDuaPuluhRibu === 0) {
-  } else {
-    console.log(`${calculateDuaPuluhRibu} lembar 20000`);
-  }
-  changeRemaining = changeRemaining % 20000;
-
-  let calculateSepuluhRibu = Math.floor(changeRemaining / 10000);
-  if (calculateSepuluhRibu === 0) {
-  } else {
-    console.log(`${calculateSepuluhRibu} lembar 10000`);
-  }
-  changeRemaining = changeRemaining % 10000;
-
-  let calculateLimaRibu = Math.floor(changeRemaining / 5000);
-  if (calculateLimaRibu === 0) {
-  } else {
-    console.log(`${calculateLimaRibu} lembar 5000`);
-  }
-  changeRemaining = changeRemaining % 5000;
-
-  let calculateDuaRibu = Math.floor(changeRemaining / 2000);
-  if (calculateDuaRibu === 0) {
-  } else {
-    console.log(`${calculateDuaRibu} lembar 2000`);
-  }
-  changeRemaining = changeRemaining % 2000;
-
-  let calculateSeribu = Math.floor(changeRemaining / 1000);
-  if (calculateSeribu === 0) {
-  } else {
-    console.log(`${calculateSeribu} koin 1000`);
-  }
-  changeRemaining = changeRemaining % 1000;
-
-  let calculateLimaRatus = Math.floor(changeRemaining / 500);
-  if (calculateLimaRatus === 0) {
-  } else {
-    console.log(`${calculateLimaRatus} koin 500`);
-  }
-  changeRemaining = changeRemaining % 500;
-
-  let calculateDuaRatus = Math.floor(changeRemaining / 200);
-  if (calculateDuaRatus === 0) {
-  } else {
-    console.log(`${calculateDuaRatus} koin 200`);
-  }
-  changeRemaining = changeRemaining % 200;
-
-  let calculateSeratus = Math.floor(changeRemaining / 100);
-  if (calculateSeratus === 0) {
-  } else {
-    console.log(`${calculateSeratus} koin 100`);
-  }
-  changeRemaining = changeRemaining % 100;
 }
 
-calculateChange(2921, 122000);
+calculateChange(2921, 1000100);
 
 // =====================================================================================================
 
